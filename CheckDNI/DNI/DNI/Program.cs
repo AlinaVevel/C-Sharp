4178810f5d2e5e06e908f4db70deef0de2fa1de2
AlinaVevel <90707109+AlinaVevel@users.noreply.github.com>
2022-05-22 16:47:56 +0200

    DniCheck
    Little program to check dni

---
+++ CheckDNI/DNI/DNI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DNI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData;
+            
+            Regex exFecha = new Regex("^[0-9]{8}$");
+            Regex salirFecha = new Regex("^0$");
+            inputData = InputData();
+           
+            
+
+            while (inputData != "0")
+            {
+                int DNI;
+                int.TryParse(inputData.Substring(0, 8), out DNI);
+                string letters = "TRWAGMYFPDXBNJZSQVHLCKE";
+                int result = DNI % 23;
+                
+                Console.WriteLine("{0}{1}",DNI,letters[result]);
+                inputData = InputData();
+
+                
+            }
+            string InputData()
+            {
+                string fecha;
+
+                do
+                {
+                    Console.Write("Introduzca la num dni, 0 para salir  ");
+                    fecha = Console.ReadLine();
+
+                } while (!exFecha.IsMatch(fecha) && !salirFecha.IsMatch(fecha));
+
+                return fecha;
+            }
+        }
+    }
+}
