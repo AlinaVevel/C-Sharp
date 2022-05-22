c72b56194944dc32daa0b67a97ab1c03be0fd85d
AlinaVevel <90707109+AlinaVevel@users.noreply.github.com>
2022-05-22 17:11:19 +0200

    Add files via upload

---
+++ ListaPersonas2/ListaPersonas/ListaPersonas/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaPersonas
+{
+    public partial class Form1 : Form
+    {
+        Persona persona = new Persona();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            persona.Dni = txtBoxDNI.Text;   
+            persona.Nombre = txtNombre.Text;
+            persona.Apellido1 = txtBoxApellino1.Text;
+            persona.Apellido2 = txtApellido2.Text;
+            
+            persona.AgregarPersona(persona);
+            dataGridView1.DataSource = persona.Refrescar();
+            Limpiar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = persona.Refrescar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string dni  = txtBoxDNI.Text;
+            
+            persona =  persona.BuscarPersona(dni);
+
+            txtNombre.Text = persona.Nombre;
+            txtBoxApellino1.Text = persona.Apellido1;
+            txtApellido2.Text = persona.Apellido2;
+            txtBoxEdad.Text = persona.Edad.ToString();
+            dataGridView1.DataSource = persona.Refrescar();
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            persona.Dni = txtBoxDNI.Text;
+            persona.Nombre = txtNombre.Text;
+            persona.Apellido1 = txtBoxApellino1.Text;
+            persona.Apellido2 = txtApellido2.Text;
+            persona.Edad = Convert.ToInt32(txtBoxEdad.Text);
+            persona.ModificarPersona(persona);
+            dataGridView1.DataSource = persona.Refrescar();
+            Limpiar();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            persona.Dni = txtBoxDNI.Text;
+            persona.BorrarPersona(persona);
+            dataGridView1.DataSource = persona.Refrescar();
+            Limpiar();
+        }
+
+        public void Limpiar()
+        {
+            txtBoxDNI.Text = "";
+            txtNombre.Text = "";
+            txtBoxApellino1.Text = "";
+            txtApellido2.Text = "";
+            txtBoxEdad.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+}
---
+++ ListaPersonas2/ListaPersonas/ListaPersonas/Persona.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPersonas
+{
+    internal class Persona
+    {
+        private string dni;
+        private string nombre;
+        private string apellido1;
+        private string apellido2;
+        private int edad;
+        OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Interfaces\ListaPersonas2\ListaPersonas\Personasaccdb.accdb");
+
+        //("^[0-9]+([,][0-9]+)?$");
+
+        public string Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+
+        }
+
+        public string Apellido1
+        {
+            get { return apellido1; }
+            set { apellido1 = value; }
+        }
+
+        public string Apellido2
+        {
+            get { return apellido2; }
+            set { apellido2 = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public DataTable Refrescar()
+        {
+            string cadena = @"Select * FROM Personas";
+            OleDbDataAdapter da = new OleDbDataAdapter(cadena, connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public void AgregarPersona(Persona persona)
+        {
+            try
+            {
+                string cadena = @"INSERT INTO Personas(Dni, Nombre, Apellido1, Apellido2, Edad) VALUES(@Dni, @Nombre, @Apellido, @Apellido2, @Edad)";
+                OleDbCommand cmd = new OleDbCommand(cadena, connection);
+                cmd.Parameters.AddWithValue("Dni", persona.dni);
+                cmd.Parameters.AddWithValue("Nombre", persona.nombre);
+                cmd.Parameters.AddWithValue("Apellido1", persona.apellido1);
+                cmd.Parameters.AddWithValue("Apellido2", persona.apellido2);
+                cmd.Parameters.AddWithValue("Edad", persona.edad);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }catch (Exception ex)
+            {
+
+            }
+           
+        }
+
+        public void ModificarPersona(Persona persona)
+        {
+            try
+            {
+                string cadena = @"UPDATE Personas SET Dni = @Dni, Nombre = @Nombre, Apellido1 = @Apellido1, Apellido2 = @Apellido2, Edad = @Edad WHERE Dni = @Dni";
+                OleDbCommand c = new OleDbCommand(cadena, connection);
+                c.Parameters.AddWithValue("Dni", persona.dni);
+                c.Parameters.AddWithValue("Nombre", persona.nombre);
+                c.Parameters.AddWithValue("Apellido1", persona.apellido1);
+                c.Parameters.AddWithValue("Apellido2", persona.apellido2);
+                c.Parameters.AddWithValue("Edad", persona.edad);
+                connection.Open();
+                c.ExecuteNonQuery();
+                connection.Close();
+            }catch(Exception ex)
+            {
+
+            }
+            
+        }
+
+        public Persona BuscarPersona(string dni)
+        {
+            Persona persona = new Persona();
+           
+               
+                string cadena = @"SELECT * FROM Personas WHERE Dni = @Dni";
+                OleDbCommand cmd = new OleDbCommand(cadena, connection);
+                cmd.Parameters.AddWithValue("Dni", dni);
+                connection.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    persona.nombre = reader["Nombre"].ToString();
+                    persona.apellido1 = reader["Apellido1"].ToString();
+                    persona.apellido2 = reader["Apellido2"].ToString();
+                    persona.edad = Convert.ToInt32(reader["Edad"].ToString());
+                }
+                connection.Close();
+           
+
+            return persona;
+        }
+
+        public void BorrarPersona(Persona persona)
+        {
+            try
+            {
+                string cadena = @"DELETE FROM Personas WHERE Dni = @Dni";
+                OleDbCommand command = new OleDbCommand(cadena, connection);
+                command.Parameters.AddWithValue("Dni", persona.dni);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }catch(Exception ex)
+            {
+
+            }
+           
+        }
+    }
+}
