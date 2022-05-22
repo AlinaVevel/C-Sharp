41d440b932efca29dcaf0757060ace382e13cd3c
AlinaVevel <90707109+AlinaVevel@users.noreply.github.com>
2022-05-22 17:14:33 +0200

    Add files via upload

---
+++ SQLProyectoBueno/AccessProyectoBueno/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace AccessProyectoBueno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvDatos = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.txbApellido1 = new System.Windows.Forms.TextBox();
+            this.txbApellido2 = new System.Windows.Forms.TextBox();
+            this.txbEdad = new System.Windows.Forms.TextBox();
+            this.txbDni = new System.Windows.Forms.TextBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.lblAp1 = new System.Windows.Forms.Label();
+            this.lblAp2 = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblDNI = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgvDatos
+            // 
+            this.dtgvDatos.AllowUserToAddRows = false;
+            this.dtgvDatos.AllowUserToDeleteRows = false;
+            this.dtgvDatos.AllowUserToResizeColumns = false;
+            this.dtgvDatos.AllowUserToResizeRows = false;
+            this.dtgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvDatos.Location = new System.Drawing.Point(44, 219);
+            this.dtgvDatos.MultiSelect = false;
+            this.dtgvDatos.Name = "dtgvDatos";
+            this.dtgvDatos.ReadOnly = true;
+            this.dtgvDatos.RowHeadersVisible = false;
+            this.dtgvDatos.Size = new System.Drawing.Size(644, 206);
+            this.dtgvDatos.TabIndex = 0;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(613, 42);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(613, 95);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 2;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(613, 156);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(502, 42);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(502, 95);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(502, 155);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // txbApellido1
+            // 
+            this.txbApellido1.Location = new System.Drawing.Point(339, 59);
+            this.txbApellido1.Name = "txbApellido1";
+            this.txbApellido1.Size = new System.Drawing.Size(100, 20);
+            this.txbApellido1.TabIndex = 7;
+            // 
+            // txbApellido2
+            // 
+            this.txbApellido2.Location = new System.Drawing.Point(339, 104);
+            this.txbApellido2.Name = "txbApellido2";
+            this.txbApellido2.Size = new System.Drawing.Size(100, 20);
+            this.txbApellido2.TabIndex = 8;
+            // 
+            // txbEdad
+            // 
+            this.txbEdad.Location = new System.Drawing.Point(339, 146);
+            this.txbEdad.Name = "txbEdad";
+            this.txbEdad.Size = new System.Drawing.Size(100, 20);
+            this.txbEdad.TabIndex = 9;
+            this.txbEdad.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txbDni
+            // 
+            this.txbDni.Location = new System.Drawing.Point(93, 79);
+            this.txbDni.Name = "txbDni";
+            this.txbDni.Size = new System.Drawing.Size(100, 20);
+            this.txbDni.TabIndex = 10;
+            this.txbDni.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(93, 122);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(100, 20);
+            this.txbNombre.TabIndex = 11;
+            // 
+            // lblAp1
+            // 
+            this.lblAp1.AutoSize = true;
+            this.lblAp1.Location = new System.Drawing.Point(242, 62);
+            this.lblAp1.Name = "lblAp1";
+            this.lblAp1.Size = new System.Drawing.Size(79, 13);
+            this.lblAp1.TabIndex = 12;
+            this.lblAp1.Text = "Primer Apellido:";
+            // 
+            // lblAp2
+            // 
+            this.lblAp2.AutoSize = true;
+            this.lblAp2.Location = new System.Drawing.Point(228, 107);
+            this.lblAp2.Name = "lblAp2";
+            this.lblAp2.Size = new System.Drawing.Size(93, 13);
+            this.lblAp2.TabIndex = 13;
+            this.lblAp2.Text = "Segundo Apellido:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(286, 149);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(35, 13);
+            this.lblEdad.TabIndex = 14;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // lblDNI
+            // 
+            this.lblDNI.AutoSize = true;
+            this.lblDNI.Location = new System.Drawing.Point(58, 82);
+            this.lblDNI.Name = "lblDNI";
+            this.lblDNI.Size = new System.Drawing.Size(29, 13);
+            this.lblDNI.TabIndex = 15;
+            this.lblDNI.Text = "DNI:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(41, 129);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 16;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 450);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblDNI);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblAp2);
+            this.Controls.Add(this.lblAp1);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.txbDni);
+            this.Controls.Add(this.txbEdad);
+            this.Controls.Add(this.txbApellido2);
+            this.Controls.Add(this.txbApellido1);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dtgvDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvDatos;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.TextBox txbApellido1;
+        private System.Windows.Forms.TextBox txbApellido2;
+        private System.Windows.Forms.TextBox txbEdad;
+        private System.Windows.Forms.TextBox txbDni;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.Label lblAp1;
+        private System.Windows.Forms.Label lblAp2;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblDNI;
+        private System.Windows.Forms.Label lblNombre;
+    }
+}
+
---
+++ SQLProyectoBueno/AccessProyectoBueno/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
+
+namespace AccessProyectoBueno
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexionConlaBD = new SqlConnection(@"server=DESKTOP-SF25OMH\SQLEXPRESS; database = SqlS_DbPersonas; integrated security = true");
+        string cadenaSql = @"SELECT * 
+                                   FROM SqlS_TaPersonas
+                                  ORDER BY Dni;";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter puenteConLaTabla = new SqlDataAdapter(cadenaSql, conexionConlaBD);
+            DataTable tablaDeLaDB = new DataTable();
+            puenteConLaTabla.Fill(tablaDeLaDB);
+            dtgvDatos.DataSource = tablaDeLaDB;
+            //LAPTOP-22N1MPUV\SQLEXPRESS
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Regex regexEdad = new Regex("[0-9]{1,2}^$");
+
+            if(regexEdad.IsMatch(txbEdad.Text))
+            {
+                txbEdad.Text = "0";
+            }      
+            string cadenaSql = @"
+                 IF NOT EXISTS (SELECT * FROM SqlS_TaPersonas WHERE Dni = @Dni)
+                   BEGIN
+                      INSERT INTO SqlS_TaPersonas
+                        (Dni, Nombre, Apellido1, Apellido2, Edad)
+                      VALUES
+                        (@Dni, @Nombre, @Apellido1, @Apellido2, @Edad)
+                   END;";
+          
+            SqlCommand instruccionesSql = new SqlCommand(cadenaSql, conexionConlaBD);
+            instruccionesSql.Parameters.AddWithValue("@Dni", txbDni.Text);
+            instruccionesSql.Parameters.AddWithValue("@Nombre", txbNombre.Text);
+            instruccionesSql.Parameters.AddWithValue("@Apellido1", txbApellido1.Text);
+            instruccionesSql.Parameters.AddWithValue("@Apellido2", txbApellido2.Text);
+            instruccionesSql.Parameters.AddWithValue("@Edad", txbEdad.Text);
+            conexionConlaBD.Open(); 
+            instruccionesSql.ExecuteNonQuery();
+            conexionConlaBD.Close();
+
+            refrescarTabla();
+            limpiarPantalla();
+          
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string cadenaSql = $@"SELECT * 
+                                   FROM SqlS_TaPersonas
+                                  WHERE Dni = @Dni;";
+
+            SqlCommand instruccionesSql = new SqlCommand(cadenaSql, conexionConlaBD);
+            instruccionesSql.Parameters.AddWithValue("@Dni", txbDni.Text);
+            conexionConlaBD.Open();
+            SqlDataReader registro = instruccionesSql.ExecuteReader();
+            if (registro.Read())
+            {
+                txbDni.Text = registro["Dni"].ToString();
+                txbNombre.Text = registro["Nombre"].ToString();
+                txbApellido1.Text = registro["Apellido1"].ToString();
+                txbApellido2.Text = registro["Apellido2"].ToString();
+                txbEdad.Text = registro["Edad"].ToString();
+            }
+            conexionConlaBD.Close();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            string dni = txbDni.Text;
+
+            string cadenaSql = $@"
+            IF EXISTS (SELECT * FROM SqlS_TaPersonas WHERE Dni = @Dni)
+              BEGIN
+
+                UPDATE SqlS_TaPersonas
+                SET
+                    Dni = @Dni,
+                    Nombre = @Nombre,
+                    Apellido1 = @Apellido1,
+                    Apellido2 = @Apellido2,
+                    Edad = @Edad
+                WHERE Dni = '{dni}'
+              END;";
+
+            SqlCommand instruccionesSql = new SqlCommand(cadenaSql, conexionConlaBD);
+            instruccionesSql.Parameters.AddWithValue("@Dni", dni);
+            instruccionesSql.Parameters.AddWithValue("@Nombre", txbNombre.Text);
+            instruccionesSql.Parameters.AddWithValue("@Apellido1", txbApellido1.Text);
+            instruccionesSql.Parameters.AddWithValue("@Apellido2", txbApellido2.Text);
+            instruccionesSql.Parameters.AddWithValue("@Edad", txbEdad.Text);
+            conexionConlaBD.Open();
+            instruccionesSql.ExecuteNonQuery();
+            conexionConlaBD.Close();
+            refrescarTabla();
+            limpiarPantalla();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string cadenaSqlEliminar = @"
+                    DELETE FROM SqlS_TaPersonas
+                    WHERE Dni = @DNI;";
+
+            SqlCommand instruccionesSql = new SqlCommand(cadenaSqlEliminar, conexionConlaBD);
+            instruccionesSql.Parameters.AddWithValue("@DNI", txbDni.Text);
+            conexionConlaBD.Open();
+            instruccionesSql.ExecuteNonQuery();
+            conexionConlaBD.Close();
+            refrescarTabla();
+            limpiarPantalla();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiarPantalla();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void refrescarTabla()
+        {
+            SqlDataAdapter puenteConLaTabla = new SqlDataAdapter(cadenaSql, conexionConlaBD);
+            DataTable tablaDeLaDB = new DataTable();
+            puenteConLaTabla.Fill(tablaDeLaDB);
+            dtgvDatos.DataSource = tablaDeLaDB;
+        }
+
+        private void limpiarPantalla()
+        {
+            txbDni.Text = "";
+            txbNombre.Text = "";
+            txbApellido1.Text = "";
+            txbApellido2.Text = "";
+            txbEdad.Text = "";
+        }
+    }
+}
